8495935b212129193cb0434f4b9623893a204125
여다희 <yeodahui.dev@gmail.com>
2023-10-29 18:27:49 +0900

    추가: 사용하지 않는 코드 정리 및 app에서 토큰 가져오기

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import "./App.css";
+import { getUserToken } from "./modules/getUserToken";
 
 function App() {
+  if (localStorage.getItem("token") === undefined) {
+    getUserToken();
+  } else {
+    console.log("토큰이 저장되어있습니다.");
+  }
   return <></>;
 }
 
